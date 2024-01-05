885ece46649592c5482819836098b277fdf09622
Vojtech Baláž <vojtikbalaz@gmail.com>
2024-01-05 14:33:06 +0100

    A lot of new stuff haha

--- GPS Compass/ViewController.swift
+++ GPS Compass/ViewController.swift
@@ -7,7 +7,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var latitudeTextField: UITextField!
     @IBOutlet weak var longitudeTextField: UITextField!
     @IBOutlet weak var arrowImageView: UIImageView!
-
+    @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet weak var myAltitudeLabel: UILabel!
+    @IBOutlet var distanceImage: UIImageView!
+    @IBOutlet var altitudeImage: UIImageView!
+    @IBOutlet var timezoneLabel: UILabel!
+    @IBOutlet var headingDirectionLabel: UILabel!
+    @IBOutlet var timezoneImage: UIImageView!
+    @IBOutlet var directionImage: UIImageView!
+    @IBOutlet var stateLabel: UILabel!
+    
     var destinationCoordinate: CLLocationCoordinate2D?
     let motionManager = CMMotionManager()
 
@@ -25,6 +34,8 @@ class ViewController: UIViewController {
         // Set background text for the text fields
         latitudeTextField.placeholder = "Insert latitude"
         longitudeTextField.placeholder = "Insert longitude"
+        
+        updateInterfaceForCurrentTraitCollection()
 
         locationManager.delegate = self
         locationManager.startUpdatingHeading() // Add this line to start heading updates
@@ -32,20 +43,66 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            let status = locationManager.authorizationStatus
-            if status == .notDetermined {
-                locationManager.requestAlwaysAuthorization()
-            } else if status == .authorizedAlways || status == .authorizedWhenInUse {
-                locationManager.startUpdatingLocation()
-            } else {
-                // Handle case where location services are denied
-                displayErrorAlert(message: "Location services are not authorized.")
-            }
+        
+        DispatchQueue.main.async {
+            self.checkLocationServices()
+        }
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        if traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            updateInterfaceForCurrentTraitCollection()
+            updateAltitudeImageForCurrentTraitCollection()
+        }
+    }
+
+    func updateInterfaceForCurrentTraitCollection() {
+        if traitCollection.userInterfaceStyle == .dark {
+            // Switch to the dark mode image for distance
+            distanceImage.image = UIImage(named: "distanceDarkMode")
+            // Use the same image for both dark and light mode for timezone
+            timezoneImage.image = UIImage(named: "timezoneDarkMode")
+            // Switch to the dark mode image for direction
+            directionImage.image = UIImage(named: "directionDarkMode")
         } else {
-            // Handle case where location services are not enabled
-            displayErrorAlert(message: "Location services are not enabled.")
+            // Switch to the original image for distance in light mode
+            distanceImage.image = UIImage(named: "distanceLightMode")
+            // Use the same image for both dark and light mode for timezone
+            timezoneImage.image = UIImage(named: "timezoneLightMode")
+            // Switch to the original image for direction in light mode
+            directionImage.image = UIImage(named: "directionLightMode")
+        }
+    }
+
+    func updateAltitudeImageForCurrentTraitCollection() {
+        if traitCollection.userInterfaceStyle == .dark {
+            // Switch to the dark mode image for altitude
+            altitudeImage.image = UIImage(named: "altitudeDarkMode")
+        } else {
+            // Switch to the original image for altitude in light mode
+            altitudeImage.image = UIImage(named: "altitudeLightMode")
+        }
+    }
+
+    func checkLocationServices() {
+        locationManager.delegate = self
+
+        let authorizationStatus = locationManager.authorizationStatus
+
+        switch authorizationStatus {
+        case .notDetermined:
+            locationManager.requestAlwaysAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            DispatchQueue.main.async {
+                self.locationManager.startUpdatingLocation()
+            }
+        case .denied, .restricted:
+            // Handle case where location services are denied or restricted
+            displayErrorAlert(message: "Location services are not authorized.")
+        @unknown default:
+            fatalError("Unhandled authorization status.")
         }
     }
 
@@ -56,6 +113,50 @@ class ViewController: UIViewController {
 
     @IBAction func confirmButtonTapped(_ sender: UIButton) {
         validateCoordinates()
+    }
+
+    func getTimeDifference(from sourceTimeZone: TimeZone, to destinationTimeZone: TimeZone) -> String {
+        let sourceOffset = sourceTimeZone.secondsFromGMT()
+        let destinationOffset = destinationTimeZone.secondsFromGMT()
+
+        let difference = (destinationOffset - sourceOffset) / 3600
+
+        if difference > 0 {
+            return String(format: "Timezone: +%d hours", difference)
+        } else if difference < 0 {
+            return String(format: "Timezone: %d hours", difference)
+        } else {
+            return "Same time zone"
+        }
+    }
+
+    func getTimeZone(for coordinates: CLLocationCoordinate2D, completionHandler: @escaping (TimeZone?) -> Void) {
+        let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("Reverse geocoding error: \(error.localizedDescription)")
+                completionHandler(nil)
+                return
+            }
+
+            guard let placemark = placemarks?.first else {
+                print("No placemark found for the given coordinates.")
+                completionHandler(nil)
+                return
+            }
+
+            if let timeZone = placemark.timeZone {
+                let userTimeZone = TimeZone.current
+                let timeDifference = self.getTimeDifference(from: userTimeZone, to: timeZone)
+
+                print("Time Difference: \(timeDifference)")
+                completionHandler(timeZone)
+            } else {
+                print("No time zone information available.")
+                completionHandler(nil)
+            }
+        }
     }
 
     private var myAngle: Double = 0.0
@@ -113,7 +214,7 @@ class ViewController: UIViewController {
                         return
                     }
 
-                    guard let motion = motion else {
+                    guard motion != nil else {
                         print("Motion data is nil.")
                         return
                     }
@@ -162,31 +263,81 @@ extension ViewController: CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         // Handle authorization changes
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.first?.coordinate else {
+        guard let location = locations.first else {
             print("Location data is nil.")
             return
         }
-
-        self.myCoordinates = location
-
+        
+        self.myCoordinates = location.coordinate
+        
         guard let destinationCoord = destinationCoordinate, let myCoord = self.myCoordinates else {
             print("Destination coordinates or my coordinates are nil.")
             return
         }
-
+        
         if self.initialNorthAngle == 0.0 {
             self.initialNorthAngle = self.calculateAngleFromNorth(heading: locationManager.heading?.trueHeading ?? 0.0,
                                                                   destinationCoord: destinationCoord,
                                                                   myCoord: myCoord)
         }
+        
+        // Calculate distance
+        let distance = calculateDistance(from: myCoord, to: destinationCoord)
+        
+        // Format distance based on magnitude
+        let formattedDistance: String
+        if distance < 1000.0 {
+            formattedDistance = String(format: "%.0f meters", distance)
+        } else {
+            formattedDistance = String(format: "%.1f kilometers", distance / 1000.0)
+        }
+        
+        // Display altitude information in the label
+        let myAltitudeText = String(format: "My Altitude: %.0f meters", location.altitude)
+        
+        // Get destination timezone and update label
+        getTimeZone(for: destinationCoord) { timeZone in
+            DispatchQueue.main.async {
+                if let timeZone = timeZone {
+                    let timeDifference = self.getTimeDifference(from: TimeZone.current, to: timeZone)
+                    self.timezoneLabel.text = "\(timeDifference)"
+                } else {
+                    self.timezoneLabel.text = "Unknown"
+                }
+            }
+        }
+        
+        // Update heading direction and label
+        if let heading = locationManager.heading?.trueHeading {
+            let headingDirection = calculateHeadingDirection(heading: heading)
+            headingDirectionLabel.text = "Direction: \(headingDirection)"
+        }
+        
+        // Update labels
+        distanceLabel.text = "Distance: \(formattedDistance)"
+        myAltitudeLabel.text = myAltitudeText
+        
+        print("Updated location: \(location.coordinate)")
+        print("My Altitude: \(location.altitude) meters")
+    }
 
-        print("Updated location: \(location)")
-        print("Updated destinationCoord: \(destinationCoord)")
-        print("Updated myCoord: \(myCoord)")
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        // Update heading direction when a new heading is available
+        if let heading = locationManager.heading?.trueHeading {
+            let headingDirection = calculateHeadingDirection(heading: heading)
+            headingDirectionLabel.text = "Direction: \(headingDirection)"
+        }
     }
 }
+
+func calculateHeadingDirection(heading: CLLocationDirection) -> String {
+        let directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"]
+
+        let index = Int((heading + 22.5) / 45.0) & 7
+        return directions[index]
+    }
 
 extension ViewController {
     func calculateBearing(from sourceCoordinate: CLLocationCoordinate2D, to destinationCoordinate: CLLocationCoordinate2D) -> CLLocationDirection {
@@ -237,4 +388,17 @@ extension CLLocationDirection {
     func normalizedDegree() -> CLLocationDirection {
         return (self + 360.0).truncatingRemainder(dividingBy: 360.0)
     }
+}
+
+func calculateDistance(from sourceCoordinate: CLLocationCoordinate2D, to destinationCoordinate: CLLocationCoordinate2D) -> CLLocationDistance {
+    let R = 6371000.0 // Earth radius in meters
+
+    let dLat = (destinationCoordinate.latitude - sourceCoordinate.latitude).toRadians()
+    let dLon = (destinationCoordinate.longitude - sourceCoordinate.longitude).toRadians()
+
+    let a = sin(dLat/2) * sin(dLat/2) + cos(sourceCoordinate.latitude.toRadians()) * cos(destinationCoordinate.latitude.toRadians()) * sin(dLon/2) * sin(dLon/2)
+    let c = 2 * atan2(sqrt(a), sqrt(1-a))
+
+    let distance = R * c
+    return distance
 }
